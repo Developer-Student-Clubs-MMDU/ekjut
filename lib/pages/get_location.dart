38d474130abc896359143a38b0a38a85550c3a85
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-02-10 00:13:14 +0530

    add get location page

--- lib/pages/get_location.dart
+++ lib/pages/get_location.dart
@@ -1,4 +1,10 @@
+import 'package:ekjut/wigets/button.dart';
+import 'package:ekjut/wigets/multi_choice.dart';
+import 'package:ekjut/wigets/red_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class GetMyLocation extends StatefulWidget {
   const GetMyLocation({Key? key}) : super(key: key);
@@ -8,8 +14,243 @@ class GetMyLocation extends StatefulWidget {
 }
 
 class _GetMyLocationState extends State<GetMyLocation> {
+  List<String> selectedMonthList = [];
+  static List<String> monthList = ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+
+  // final DateTime _dateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    final _height = MediaQuery.of(context).size.height;
+    final _width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+          body: Container(
+        width: _width,
+        height: _height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                const Color(0xFF36306D).withOpacity(1),
+                const Color(0xFF1C173D)
+              ]),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceEvenly, // use whichever suits your need
+
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("From:",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.w800,
+                          )),
+                      SizedBox(
+                        width: _width * 0.4,
+                        height: _height * 0.2,
+                        // color: Colors.green,
+                        child: Expanded(
+                          child: CupertinoDatePicker(
+                            mode: CupertinoDatePickerMode.time,
+                            onDateTimeChanged: (value) {
+                              // if (value != null && value != selectedDate)
+                              //   setState(() {
+                              //     selectedDate = value;
+
+                              //   });
+                            },
+                            initialDateTime: DateTime.now(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("To:",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.w800,
+                          )),
+                      SizedBox(
+                        width: _width * 0.4,
+                        height: _height * 0.2,
+                        // color: Colors.green,
+                        child: Expanded(
+                          child: CupertinoDatePicker(
+                            mode: CupertinoDatePickerMode.time,
+                            onDateTimeChanged: (value) {
+                              // if (value != null && value != selectedDate)
+                              //   setState(() {
+                              //     selectedDate = value;
+
+                              //   });
+                            },
+                            initialDateTime: DateTime.now(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              MultiSelectChip(
+                monthList,
+                onSelectionChanged: (selectedList) {
+                  setState(() {
+                    selectedMonthList = selectedList;
+                    // print(selectedMonthList);
+                  });
+                },
+                maxSelection: 7,
+              ),
+              const SizedBox(height: 14),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Select the path:",
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontFamily: "Roboto",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 12),
+                    ),
+                    Icon(
+                      FontAwesomeIcons.infoCircle,
+                      size: 18,
+                      color: Colors.grey[400],
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const GetMyLocation()));
+                },
+                child: Container(
+                  height: 60,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    color: const Color(0xFF1C173D),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        spreadRadius: 0,
+                        blurRadius: 4,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.map,
+                          size: 18,
+                          color: Colors.grey[400],
+                        ),
+                        const SizedBox(width: 14),
+                        Text(
+                          "From",
+                          style: TextStyle(
+                              color: Colors.grey[400],
+                              fontFamily: "Roboto",
+                              fontWeight: FontWeight.w700,
+                              fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 14),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const GetMyLocation()));
+                },
+                child: Container(
+                  height: 60,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    color: const Color(0xFF1C173D),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        spreadRadius: 0,
+                        blurRadius: 4,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.map,
+                          size: 18,
+                          color: Colors.grey[400],
+                        ),
+                        const SizedBox(width: 14),
+                        Text(
+                          "To",
+                          style: TextStyle(
+                              color: Colors.grey[400],
+                              fontFamily: "Roboto",
+                              fontWeight: FontWeight.w700,
+                              fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RedButton(
+                    width: _width * 0.4,
+                    label: "Cancel",
+                    onPress: () {},
+                  ),
+                  ButtonWidget(
+                    width: _width * 0.4,
+                    label: "Save",
+                    onPress: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
